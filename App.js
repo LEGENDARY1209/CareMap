52a34bde46daa4c628510f6692d8bdd9e3fdc03b
Raí <130669909+LEGENDARY1209@users.noreply.github.com>
2024-09-06 16:26:17 -0300

    commit do rai

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
+import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import logoImage from './assets/LogoEmpire.png'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <Image style={styles.logo} source={logoImage}/>
+    <Text= style{styles.titleText}>CareMap</Text</>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +16,17 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#161616',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  logo: {
+    height: 64,
+    width: 64
+  },
+  titleText: {}
+  color: '#fff',
+  fontSize: 24,
+  fontWeight: 'bold',
+  }
 });
